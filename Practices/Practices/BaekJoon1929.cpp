14c41bf4b68bd34ab5e783dc979563e33480d8e4
hyerin <lhr_id@naver.com>
2020-01-26 23:49:01 +0900

    PrimeNumber (1 ≤ M ≤ N )

---
+++ Practices/Practices/BaekJoon1929.cpp
@@ -0,0 +1,11 @@
+/*
+M이상 N이하의 소수를 모두 출력하는 프로그램을 작성하시오.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	return 0;
+}
--- Practices/Practices/Baekjoon1978cpp.cpp
+++ Practices/Practices/Baekjoon1978cpp.cpp
@@ -28,7 +28,7 @@ bool CheckPrime(int num)
 {
 	if (num == 1) return false;
 
-	for (int i = 2; i < sqrt(num); ++i)
+	for (int i = 2; i <= sqrt(num); ++i)
 	{
 		if (num % i == 0) // 소수라면
 		{
@@ -37,6 +37,3 @@ bool CheckPrime(int num)
 	}
 	return true;
 }
-
-
-
