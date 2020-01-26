26d6520b2021c05684d1b4f9432eaebc2e5578b0
hyerin <lhr_id@naver.com>
2020-01-27 00:33:41 +0900

    simply Check Prime Number

--- Practices/Practices/BaekJoon1929.cpp
+++ Practices/Practices/BaekJoon1929.cpp
@@ -3,9 +3,36 @@ M이상 N이하의 소수를 모두 출력하는 프로그램을 작성하시오.
 */
 
 #include <iostream>
-using namespace std;
+#include <math.h>
+
+int CheckPrime(int);
 
 int main(void)
 {
+	int m, n, t{ 0 };
+
+	std::cin >> m >> n;
+
+	for (int i = m; i <= n; ++i)
+	{
+		t = CheckPrime(i);
+
+		if (t != 0) std::cout << t << std::endl;
+	}
+
 	return 0;
+}
+
+int CheckPrime(int num)
+{
+	if (num == 1) return 0;
+
+	for (int i = 2; i <= sqrt(num); ++i)
+	{
+		if (num % i == 0) // 소수가 아니라면
+		{
+			return 0;
+		}
+	}
+	return num; //소수라면
 }
