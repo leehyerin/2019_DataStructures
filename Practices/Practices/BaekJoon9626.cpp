03bdd7ae8ed6e67fa0345454d212dc5d42aa9bb7
leehyerin <lhr_idnaver.com>
2020-06-18 17:11:08 +0900

    matrix

--- Practices/Practices/BaekJoon9626.cpp
+++ Practices/Practices/BaekJoon9626.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+<<<<<<< HEAD
+=======
+#include <string>
+>>>>>>> 428b2e1... matrix
 using namespace std;
 
 int main(void)
@@ -9,6 +13,7 @@ int main(void)
 
 	cin >> M >> N >> U >> L >> R >> D;
 
+<<<<<<< HEAD
 	int width = L + N + R;
 	int height = U + M + D;
 	
@@ -27,6 +32,34 @@ int main(void)
 	{
 		for (r = 0; r < width; ++r)
 			cout << board[c][r];
+=======
+	int width = N + L + R;
+	int height = U + M + D;
+	
+	int c, r;
+	for (c = 0; c < width; ++c)
+	{
+		for (r = 0; r < height; ++r)
+		{
+			board[c][r] = ch[(r + c) % 2];
+		}
+	}
+	//퍼즐 입력
+	for (c = U; c < U + M; ++c)
+	{
+		string s;		cin >> s;
+
+		for (int i = 0; i < N; ++i)
+			board[c][L+i] = s[i];
+	}
+	// 출력
+	for (int c = 0; c < width; ++c)
+	{
+		for (int r = 0; r < height; ++r)
+		{
+			cout << board[c][r] << " ";
+		}
+>>>>>>> 428b2e1... matrix
 		cout << "\n";
 	}
 }
