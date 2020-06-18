0c5ad49a0792e79c2cfab2d2cbce6c2b72125202
leehyerin <lhr_id@naver.com>
2020-06-18 17:11:08 +0900

    matrix

---
+++ Practices/Practices/BaekJoon3079.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void)
+{
+	unsigned int N, M; //N 심사대 수, M 사람 수
+	cin >> N >> M;
+	vector<unsigned int> T(N);
+
+
+
+
+	return 0;
+}
