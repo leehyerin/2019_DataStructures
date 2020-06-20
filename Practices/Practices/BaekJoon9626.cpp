29a4e85fdf7328d94e425089dc5d628df8796743
leehyerin <ta722blo@gmail.com>
2020-06-20 14:36:33 +0900

    greedy algorithm

--- Practices/Practices/BaekJoon9626.cpp
+++ Practices/Practices/BaekJoon9626.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main(void)
@@ -10,32 +9,24 @@ int main(void)
 
 	cin >> M >> N >> U >> L >> R >> D;
 
-	int width = N + L + R;
+	int width = L + N + R;
 	int height = U + M + D;
 	
 	int c, r;
-	for (c = 0; c < width; ++c)
-	{
-		for (r = 0; r < height; ++r)
-		{
-			board[c][r] = ch[(r + c) % 2];
-		}
-	}
-	//퍼즐 입력
+	for (c = 0; c < height; ++c)
+		for (r = 0; r < width; ++r)
+			board[c][r] = ch[(r + c) % 2];  //번갈아가면서 찍히도록 # .
+	
+	//퍼즐 문자 입력
 	for (c = U; c < U + M; ++c)
-	{
-		string s;		cin >> s;
+		for (int r = L; r < L + N; ++r)
+			cin >> board[c][r];
 
-		for (int i = 0; i < N; ++i)
-			board[c][L+i] = s[i];
-	}
 	// 출력
-	for (int c = 0; c < width; ++c)
+	for (c = 0; c < height; ++c)
 	{
-		for (int r = 0; r < height; ++r)
-		{
-			cout << board[c][r] << " ";
-		}
+		for (r = 0; r < width; ++r)
+			cout << board[c][r];
 		cout << "\n";
 	}
 }
