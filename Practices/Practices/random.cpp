6d4935ed8cf3db7719c3575e6874285217064f78
leehyerin <lhr_id@naver.com>
2020-06-20 15:33:09 +0900

    sort

---
+++ Practices/Practices/random.cpp
@@ -0,0 +1,13 @@
+//#include <random>
+//#include <time.h>
+//#include <iostream>
+//
+//int main(void)
+//{
+//	std::random_device randDeivce;
+//	std::default_random_engine randEngine{ randDeivce() };
+//
+//	std::uniform_int_distribution<int> dis(0, 1000);
+//	for (int i = 0; i < 1000; ++i)
+//		std::cout << dis(randEngine) << ' ';
+//}
